244c7938feb44a42618350a59acb73b256222123
Nikola Loncarski <nidzafrom023@gmail.com>
2024-02-26 15:06:48 +0100

    GetProductsBySerialNumber controller

--- WWDemo.Data/Products/ProductRepository.cs
+++ WWDemo.Data/Products/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using WWDemo.Models;
 
 namespace WWDemo.Data.Products
@@ -44,6 +45,12 @@ namespace WWDemo.Data.Products
         {
             var products = _apiDbContext.Products;
 
+            return products;
+        }
+
+        public async Task<Product> GetProductBySerialNumber(string serialNumber)
+        {
+            var products = await GetQueryable().Where(s=>s.SerialNumber == serialNumber).FirstOrDefaultAsync();
             return products;
         }
     }
